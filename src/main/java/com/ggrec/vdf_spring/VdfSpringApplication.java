2d5dea93a7fb40fd39d11d3ea35159cd065089a6
Georgian Grec <georgian.grec@gmail.com>
2018-06-26 11:41:59 +0300

    Initial commit

---
+++ src/main/java/com/ggrec/vdf_spring/VdfSpringApplication.java
@@ -0,0 +1,12 @@
+package com.ggrec.vdf_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdfSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdfSpringApplication.class, args);
+	}
+}
